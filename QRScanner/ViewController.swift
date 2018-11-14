b0d6b522b5a597ed37d8353c5c443d589418e789
dNESHGARG <dinesh@automatic.com>
2018-11-13 20:26:02 -0800

    Initial Commit

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Dinesh Garg on 11/13/18.
+//  Copyright © 2018 Dinesh Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
